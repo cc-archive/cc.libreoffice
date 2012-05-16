eb37487bcce87418689a9ada66ee66a17f70902f
Ishan Thilina Somasiri <ishan@ishans.info>
2012-05-16 22:29:46 +0530

    Found the way to catch the GUI signals and use them

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ from com.sun.star.task import XJobExecutor
 
 
 
-class Import( unohelper.Base, XJobExecutor ):
+class SetSharing( unohelper.Base, XJobExecutor ):
     def __init__( self, ctx ):
         self.ctx = ctx
         pass
@@ -20,10 +20,28 @@ class Import( unohelper.Base, XJobExecutor ):
         # retrieve the desktop object
         desktop = self.ctx.ServiceManager.createInstanceWithContext(
             "com.sun.star.frame.Desktop", self.ctx )
+
+        print 'hi from set sharing!'
+        print args
      
         
 
-	
+class InsertSharing( unohelper.Base, XJobExecutor ):
+    def __init__( self, ctx ):
+        self.ctx = ctx
+        pass
+        
+
+    def trigger( self, args ):
+        
+        # retrieve the desktop object
+        desktop = self.ctx.ServiceManager.createInstanceWithContext(
+            "com.sun.star.frame.Desktop", self.ctx )
+
+        print 'hi from InsertSharing!'
+        print args
+     
+        	
        
 
 
@@ -31,8 +49,14 @@ class Import( unohelper.Base, XJobExecutor ):
             
 g_ImplementationHelper = unohelper.ImplementationHelper()
 g_ImplementationHelper.addImplementation(
-        Import,
+        InsertSharing,
         "org.creativecommons.openoffice.ccooo.Import",
         ("com.sun.star.task.Job",),)
 
 
+            
+#g_ImplementationHelper = unohelper.ImplementationHelper()
+g_ImplementationHelper.addImplementation(
+        SetSharing,
+        "org.creativecommons.openoffice.ccooo.SetSharing",
+        ("com.sun.star.task.Job",),)
