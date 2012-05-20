b547d0fb52a0633a8bdd42ed6982c4412a4d8400
Ishan Thilina Somasiri <ishan@ishans.info>
2012-05-20 10:55:41 +0530

    Edited the build.sh file to auotamate the build process completely.

--- build.sh
+++ build.sh
@@ -1,2 +1,17 @@
+#!/bin/bash
+
+rm out.oxt
 zip -r test * -x *.*~ build.sh */*.*~ */*/*.*~ 
 mv test.zip out.oxt
+
+#Removes the extension if it is already installed
+/usr/lib/libreoffice/program/unopkg remove org.creativecommons.openoffice.CcOOoAddin
+
+#Adds the new extension
+/usr/lib/libreoffice/program/unopkg add out.oxt
+
+#Starts LibreOffice
+libreoffice --writer
+
+#Removes the extension when quitting
+/usr/lib/libreoffice/program/unopkg remove org.creativecommons.openoffice.CcOOoAddin
