7020de32dbf1cfe3d05669e40491b1c2ca736c90
Ishan Thilina Somasiri <ishan@ishans.info>
2012-06-09 14:42:17 +0530

    Added the AcceptWaiveListener class. Didn't test.

---
+++ pythonpath/org/creativecommons/libreoffice/ui/license/AcceptWaiveListener.py
@@ -0,0 +1,108 @@
+from com.sun.star.awt.XItemListener import XItemListener
+from com.sun.star.beans.PropertyVetoException import PropertyVetoException
+from com.sun.star.beans.UnknownPropertyException import UnknownPropertyException
+from com.sun.star.container.NoSuchElementException import NoSuchElementException
+from com.sun.star.lang.WrappedTargetException import WrappedTargetException
+from com.sun.star.lang.IllegalArgumentException import IllegalArgumentException
+
+from LicenseChooserDialog import LicenseChooserDialog
+
+class AcceptWaiveListener(XItemListener):
+    """Enable CC0 deed and territory etc. after accepting to waive.
+    """
+    
+    def __init__(self, dialog):
+        """
+        
+        Arguments:
+        - `dialog`:LicenseChooserDialog
+        """
+        self.dialog = dialog
+
+    def disposing(self, arg0):
+        """
+        
+        Arguments:
+        - `arg0`: EventObject
+        """
+        pass
+
+    #@override
+    def itemStateChanged(self, event):
+        """
+        
+        Arguments:
+        - `event`:ItemEvent
+        """
+        waive=event.source
+
+        try:
+            #enable disable dialog controls accoring to the state
+            if (waive.getState()==0):
+                self.dialog.xNameCont.getByName(
+                    LicenseChooserDialog.CHK_YES_CC0).setPropertyValue(
+                        "Enabled", False)
+
+                ##TODO: was (short)0
+                self.dialog.xNameCont.getByName(LicenseChooserDialog.CHK_YES_CC0).setPropertyValue("State", 0)
+
+                self.dialog.xNameCont.getByName(
+                    LicenseChooserDialog.TXT_LEGAL_CODE_CC0).setPropertyValue(
+                        "Enabled", False)
+
+                self.dialog.xNameCont.getByName(
+                    LicenseChooserDialog.CMB_TERRITORY).setPropertyValue(
+                        "Enabled", False)
+
+                self.dialog.xNameCont.getByName(
+                    LicenseChooserDialog.BTN_OK).setPropertyValue(
+                        "Enabled", False)
+
+            else:
+                 self.dialog.xNameCont.getByName(
+                     LicenseChooserDialog.CHK_YES_CC0).setPropertyValue(
+                         "Enabled", True)
+
+                 self.dialog.xNameCont.getByName(
+                     LicenseChooserDialog.TXT_LEGAL_CODE_CC0).setPropertyValue(
+                         "Enabled", True)
+
+                 self.dialog.xNameCont.getByName(
+                     LicenseChooserDialog.CMB_TERRITORY).setPropertyValue(
+                         "Enabled", True)
+
+                 
+
+        except IllegalArgumentException, ex:
+            print "Exception in AcceptWaiveListener.itemStateChanged: "
+            print ex
+            print type(ex)
+            raise ex
+            
+
+        except WrappedTargetException,ex:
+            print "Exception in AcceptWaiveListener.itemStateChanged: "
+            print ex
+            print type(ex)
+            raise ex
+
+        except PropertyVetoException, ex:
+            print "Exception in AcceptWaiveListener.itemStateChanged: "
+            print ex
+            print type(ex)
+            raise ex
+
+        except UnknownPropertyException, ex:
+            print "Exception in AcceptWaiveListener.itemStateChanged: "
+            print ex
+            print type(ex)
+            raise ex
+
+        except NoSuchElementException, ex:
+            print "Exception in AcceptWaiveListener.itemStateChanged: "
+            print ex
+            print type(ex)
+            raise ex
+    
+
+        
