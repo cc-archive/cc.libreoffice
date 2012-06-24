238016c7162eb1d6323f375dfe84dc716f0e0bd1
Ishan Thilina Somasiri <ishan@ishans.info>
2012-06-24 11:49:08 +0530

    added the Jurisdiction class.Didn't test it under LibreOffice env

---
+++ pythonpath/org/creativecommons/license/Jurisdiction.py
@@ -0,0 +1,67 @@
+#Author: Ishan Thilina Somasiri
+#E-mail: ishan@ishans.info
+#Blog: www.blog.ishans.info
+
+from org.creativecommons.license.Store import Store
+from rdflib import Namespace
+
+class Jurisdiction():
+    """
+    """
+
+    
+    
+    def __init__(self, uri):
+        """
+        
+        Arguments:
+        - `uri`:
+        """
+        self.uri = uri
+
+    @staticmethod
+    def byId(id):
+        """
+        
+        Arguments:
+        - `id`:String
+        """
+        #TODO:This was a class wide variable
+        JURIS_BASE = "http://creativecommons.org/international/"
+        return Jurisdiction(JURIS_BASE + id + "/")
+
+    def __repr__(self):
+        return self.uri
+
+    # def getTitle(self, ):
+    #     """
+    #     """
+    #     return self.getTitle("en")
+        
+    def getTitle(self, lang=None):
+        """
+        
+        Arguments:
+        - `lang`:String
+        """
+
+        if lang is None:
+            lang="en"
+            
+        DC= Namespace("http://purl.org/dc/elements/1.1/")
+        title=Store().literal(self.uri,DC['title'],lang)
+
+        if title:
+            return str(title)
+
+        return ""
+
+    def compareTo(self, other):
+        """
+        
+        Arguments:
+        - `other`:Jurisdiction
+        """
+        
+        return cmp(self.getTitle(),other.getTitle())
+
