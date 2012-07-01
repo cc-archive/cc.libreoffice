e5124a31159b91932af7dd585f784f3125872e43
Ishan Thilina Somasiri <ishan@ishans.info>
2012-07-01 21:39:05 +0530

    Completed the license class.

--- pythonpath/org/creativecommons/license/License.py
+++ pythonpath/org/creativecommons/license/License.py
@@ -3,14 +3,17 @@
 #Blog: www.blog.ishans.info
 
 from rdflib import Namespace
+import traceback
 
 from org.creativecommons.license.Store import Store
 from org.creativecommons.license.Unported import Unported
+from org.creativecommons.license.Jurisdiction import Jurisdiction
 
 class License():
     """
     """
     CC=Namespace("http://creativecommons.org/ns#")
+    DCTerms=Namespace("http://purl.org/dc/terms/")
     
     def __init__(self, license_uri,territory=None):
         """
@@ -36,10 +39,10 @@ class License():
         #TODO: implement the Null pointer Exception handling 
 
         DC= Namespace("http://purl.org/dc/elements/1.1/")
-        DCTerms=Namespace("http://purl.org/dc/terms/")
+        
         
         return str(self.licenseStore.literal(self.license_uri,DC['title'],"en"))+""
-        +str(self.licenseStore.literal(self.license_uri,DCTerms['hasVersion'],""))+""
+        +str(self.licenseStore.literal(self.license_uri,self.DCTerms['hasVersion'],""))+""
         +self.getJurisdiction().getTitle()
 
 
@@ -69,3 +72,38 @@ class License():
         """
         """
         return self.licenseStore.exists(self.license_uri,self.CC['permits'],self.CC['DerivativeWorks'])
+
+    def getCode(self, ):
+        """Return the license code for this License.  For example, the code for the
+        Attribution 3.0 license (http://creativecommons.org/licenses/by/3.0/) is
+        "by".  Note this is based on a Creative Commons-specific standard.
+        """
+        try:
+            return self.license_uri.split('/')[4]
+        except Exception, ex:
+            traceback.print_exc()
+
+    def getVersion(self, ):
+        """License version
+        """
+
+        version=self.licenseStore.literal(self.license_uri,
+                                             self.DCTerms['hasVersion'],"")
+        if version is None:
+            return None
+        
+        return str(version)
+
+    def getImageUrl(self, ):
+        """
+        """
+        version=self.getVersion()
+
+        if version is not None:
+            return ("http://i.creativecommons.org/l/" + self.getCode() + "/"
+                    + version + "/88x31.png")
+
+        else:
+            return ("http://i.creativecommons.org/l/" + self.getCode() + "/88x31.png")
+
+    
