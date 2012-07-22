5e9e95aff8720de327e05189910214f102fbf847
Ishan Thilina Somasiri <ishan@ishans.info>
2012-07-22 09:19:09 +0530

    Applied pep to pythonpath/org/creativecommons/libreoffice/ui/license/territoryselectlistener.py

--- pythonpath/org/creativecommons/libreoffice/ui/license/territoryselectlistener.py
+++ pythonpath/org/creativecommons/libreoffice/ui/license/territoryselectlistener.py
@@ -5,23 +5,19 @@
 import unohelper
 from com.sun.star.awt import XItemListener
 
-class TerritorySelectListener(XItemListener,unohelper.Base):
+
+class TerritorySelectListener(XItemListener, unohelper.Base):
     """Get the user selected territory.
     """
-    
     def __init__(self, dialog):
         """
-        
         Arguments:
         - `dialog`:LicenseChooserDialog
         """
-        
         self.dialog = dialog
-        
-        
+
     def itemStateChanged(self, event):
         """
-        
         Arguments:
         - `event`:ItemEvent
         """
@@ -29,7 +25,6 @@ class TerritorySelectListener(XItemListener,unohelper.Base):
 
     def disposing(self, arg0):
         """
-        
         Arguments:
         - `arg0`:com.sun.star.lang.EventObject
         """
