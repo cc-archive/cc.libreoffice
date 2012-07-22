bbe39f206ba5ae0514c90a4675009517009eb6ea
Ishan Thilina Somasiri <ishan@ishans.info>
2012-07-22 09:16:28 +0530

    Applied pep to pythonpath/org/creativecommons/libreoffice/ui/license/okclicklistener.py

--- pythonpath/org/creativecommons/libreoffice/ui/license/okclicklistener.py
+++ pythonpath/org/creativecommons/libreoffice/ui/license/okclicklistener.py
@@ -7,41 +7,32 @@ import traceback
 
 from com.sun.star.awt import XActionListener
 
-class OKClickListener(XActionListener,unohelper.Base):
+
+class OKClickListener(XActionListener, unohelper.Base):
     """
     """
-    
     def __init__(self, dialog):
         """
-        
         Arguments:
         - `dialog`:LicenseChooserDialog
         """
         self.dialog = dialog
 
-
     def actionPerformed(self, aEvent):
         """
-        
         Arguments:
         - `aEvent`:ActionEvent
         """
-        
         try:
-            self.dialog.cancelled=False
+            self.dialog.cancelled = False
             self.dialog.close()
         except Exception, e:
             traceback.print_exc()
             raise e
 
-
-
     def disposing(self, eObject):
         """
-        
         Arguments:
         - `eObject`:EventObject
         """
         pass
-
-        
