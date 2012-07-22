e82256e4981945e25d7db40b5db0dd38b8ef4968
Ishan Thilina Somasiri <ishan@ishans.info>
2012-07-22 09:38:42 +0530

    Applied pep to pythonpath/org/creativecommons/libreoffice/util/shapehelper.py

--- pythonpath/org/creativecommons/libreoffice/util/shapehelper.py
+++ pythonpath/org/creativecommons/libreoffice/util/shapehelper.py
@@ -7,50 +7,49 @@ import traceback
 from com.sun.star.text.ControlCharacter import PARAGRAPH_BREAK
 from com.sun.star.text.TextContentAnchorType import AS_CHARACTER
 
-def createShape(xDrawDoc,aPos,aSize,sShapeType):
+
+def createShape(xDrawDoc, aPos, aSize, sShapeType):
     """create a Shape
-    
+
     Arguments:
     - `xDrawDoc`:XComponent
     - `aPos`:Point
     - `aSize`:Size
     - `sShapeType`:String
     """
-    
     try:
-	#xFactory=xDrawDoc
-        xShape=xDrawDoc.createInstance( sShapeType )
-        xShape.setPosition( aPos )
-        xShape.setSize( aSize )
+        #xFactory=xDrawDoc
+        xShape = xDrawDoc.createInstance(sShapeType)
+        xShape.setPosition(aPos)
+        xShape.setSize(aSize)
         return xShape
     except Exception, e:
-	traceback.print_exc()
-	raise e
+        traceback.print_exc()
+        raise e
 
-def addPortion(xShape,sText,bNewParagraph):
+
+def addPortion(xShape, sText, bNewParagraph):
     """add text to a shape. the return value is the PropertySet
         of the text range that has been added
-    
-    Arguments:
-    - `xShape`:XShape-
-    - `sText`:String
-    - `bNewParagraph`:Boolean
-    """
-    
+
+        Arguments:
+        - `xShape`:XShape-
+        - `sText`:String
+        - `bNewParagraph`:Boolean
+        """
     try:
-	#xText=xShape
+        #xText=xShape
         xTextCursor = xShape.createTextCursor()
-        xTextCursor.gotoEnd( False )
+        xTextCursor.gotoEnd(False)
 
         if bNewParagraph:
-            xShape.insertControlCharacter( xTextCursor,PARAGRAPH_BREAK, False )
-            xTextCursor.gotoEnd( False )
+            xShape.insertControlCharacter(xTextCursor, PARAGRAPH_BREAK, False)
+            xTextCursor.gotoEnd(False)
 
         #xTextRange=xTextCursor
-        xTextCursor.setString( sText )
-        xTextCursor.gotoEnd( True )
+        xTextCursor.setString(sText)
+        xTextCursor.gotoEnd(True)
         return xTextCursor
     except Exception, e:
-	traceback.print_exc()
-	#raise e
-
+        traceback.print_exc()
+        #raise e
